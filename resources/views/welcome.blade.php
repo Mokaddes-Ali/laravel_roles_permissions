0bb4fcc971a23f3e749f65bdcd8111f4022d55b5
mokadde <mokaddes.ru2000@gmail.com>
2024-08-23 10:21:10 +0600

    Second Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -130,9 +130,9 @@
                                 </div>
 
                                 <div class="pt-3 sm:pt-5">
-                                    <h2 class="text-xl font-semibold text-black dark:text-white">Laravel News</h2>
+                                   
 
-                                 
+
                                 </div>
 
                                 <svg class="size-6 shrink-0 self-center stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
